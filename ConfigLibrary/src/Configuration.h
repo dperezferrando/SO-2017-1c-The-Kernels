aaadbd4bce4a95e670704974a70b5a7bd5902825
utnso <NicolasStupfler@gmail.com>
2017-05-05 18:34:34 -0300

    Struct PCB 2.0 (falta indice de etiquetas)

--- ConfigLibrary/src/Configuration.h
+++ ConfigLibrary/src/Configuration.h
@@ -11,3 +11,36 @@ bool archivoConfigValido(t_config* configHandler,char* []);
 bool archivoConfigCompleto(t_config* configHandler, char* []);
 void* configurate(char* ,void*(t_config*), char* []);
 
+typedef struct indCod{
+	int* pagina;
+	int* offset;
+	int* longitud;
+}indCod;
+
+typedef struct IDIndCod{
+	int ID;
+	int* pagina;
+	int* offset;
+	int* longitud;
+}IDIndCod;
+
+typedef struct indEtq{
+	//diccionario de etiquetas a posicion de etiqueta
+}indEtq;
+
+typedef struct indStk{
+	IDIndCod argumentos;
+	IDIndCod variables;
+	int  posicionDeRetorno;
+	indCod variableDeRetorno;
+}indStk;
+
+typedef struct PCB{
+	int identificadorProceso;
+	int programCounter;
+	int cantPaginasCodigo;
+	indCod indiceCodigo;
+	indEtq indiceEtiqueta;
+	indStk indiceStack;
+	int exitCode;
+}PCB;
--- Kernel/src/Process.c
+++ Kernel/src/Process.c
@@ -1,8 +1,9 @@
-#include "Process.h"
+#include "Configuration.h"
+#include <commons/collections/list.h>
 
 int createProcess(t_list* procesos){//devuelve el index en la lista, que coincide con el PID
 	PCB pcb;
-	pcb.PID= list_size(procesos);
+	pcb.identificadorProceso= list_size(procesos);
 	list_add(procesos,&pcb);
-	return pcb.PID;
+	return pcb.identificadorProceso;
 }
