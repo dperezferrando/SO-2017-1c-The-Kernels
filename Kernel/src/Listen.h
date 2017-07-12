1c395331260618bc656e88411540a68de7483547
FischerMatias <mfischer00@gmail.com>
2017-07-12 17:04:49 -0300

    ff

--- Kernel/src/Listen.h
+++ Kernel/src/Listen.h
@@ -12,7 +12,7 @@
 #define PUERTO2 "7172"
 #define LOCALHOST "192.168.3.30"
 //#define LOCALHOST "127.0.0.1"
-#define BACKLOG 10
+#define BACKLOG 10 //aa
 
 void handler();
 void destroyConnHandler(connHandle*);
