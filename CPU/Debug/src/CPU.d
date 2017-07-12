3937e507a4767b0d4812ce661e8c33226251e503
FischerMatias <mfischer00@gmail.com>
2017-07-12 16:51:43 -0300

    a

--- CPU/Debug/src/CPU.d
+++ CPU/Debug/src/CPU.d
@@ -1,11 +1,8 @@
-src/CPU.d: ../src/CPU.c \
- /home/utnso/workspace/tp-2017-1c-The-Kernels/SocketLibrary/src/SocketLibrary.h \
- ../src/CPU.h ../src/../../ConfigLibrary/src/Configuration.c \
+src/CPU.d: ../src/CPU.c ../src/CPU.h \
+ ../src/../../ConfigLibrary/src/Configuration.c \
  ../src/../../ConfigLibrary/src/Configuration.h \
  ../src/../../SocketLibrary/src/SocketLibrary.c \
  ../src/../../SocketLibrary/src/SocketLibrary.h
-
-/home/utnso/workspace/tp-2017-1c-The-Kernels/SocketLibrary/src/SocketLibrary.h:
 
 ../src/CPU.h:
 
