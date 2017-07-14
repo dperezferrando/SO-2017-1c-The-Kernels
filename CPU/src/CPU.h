ffe93b2f14d592ec9d1c15a8d73f2b363d8e9344
DPoma <darioadrian96@gmail.com>
2017-07-13 22:33:18 -0300

    quantum sleep falta el inotify

--- CPU/src/CPU.h
+++ CPU/src/CPU.h
@@ -14,6 +14,7 @@
 #include "../../ConfigLibrary/src/Configuration.c"
 #include "../../SocketLibrary/src/SocketLibrary.c"
 #include <signal.h>
+#include <unistd.h>
 
 
 //DEFINES DE MEMORIA
@@ -81,6 +82,7 @@ int stackSize;
 int estado;
 t_log*  logFile;
 int toBeKilled;
+int quantumSleep;
 // ANSISOP
 t_puntero definirVariable(t_nombre_variable);
 t_puntero obtenerPosicionVariable(t_nombre_variable);
