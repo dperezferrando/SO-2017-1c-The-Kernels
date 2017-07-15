7ecb8ebc4b8e2de6306627899c5cf948957eb738
utnso <NicolasStupfler@gmail.com>
2017-07-14 21:05:35 -0300

    path desde escritorio

--- Memoria/Debug/src/Memoria.d
+++ Memoria/Debug/src/Memoria.d
@@ -1,19 +1,11 @@
 src/Memoria.d: ../src/Memoria.c \
-<<<<<<< HEAD
- /home/utnso/tp-2017-1c-The-Kernels/ConfigLibrary/src/Configuration.h \
-=======
  /home/utnso/Escritorio/tp-2017-1c-The-Kernels/ConfigLibrary/src/Configuration.h \
->>>>>>> 633dc3827af2602ac61f156326da457cb10ba6ad
  ../src/Memoria.h ../src/../../ConfigLibrary/src/Configuration.c \
  ../src/../../ConfigLibrary/src/Configuration.h \
  ../src/../../SocketLibrary/src/SocketLibrary.c \
  ../src/../../SocketLibrary/src/SocketLibrary.h
 
-<<<<<<< HEAD
-/home/utnso/tp-2017-1c-The-Kernels/ConfigLibrary/src/Configuration.h:
-=======
 /home/utnso/Escritorio/tp-2017-1c-The-Kernels/ConfigLibrary/src/Configuration.h:
->>>>>>> 633dc3827af2602ac61f156326da457cb10ba6ad
 
 ../src/Memoria.h:
 
